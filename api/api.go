503e9604f6865158c557454fc97aff0909022e48
Abduazim <yusupovabduazim0811@gmail.com>
2024-07-19 06:43:01 +0500

    first

--- api/api.go
+++ api/api.go
@@ -6,19 +6,10 @@ import (
 	"Task/internal/rabbitmq"
 	"log"
 
-	ginSwagger "github.com/swaggo/gin-swagger"
-    swaggerFiles "github.com/swaggo/files"
-	_ "Task/cmd/docs"
 
 	"github.com/gin-gonic/gin"
 )
-// New ...
-// @title	Project: Swagger Intro
-// @description This swagger UI was created in lesson
-// @version 1.0
-// @securityDefinitions.apikey Bearerauth
-// @in header
-// @name Authorization
+
 func Routes(){
 	router := gin.Default()
 	db,err := mongodb.NewTask()
@@ -31,9 +22,6 @@ func Routes(){
 	router.GET("/tasks", taskhandler.GetTasks)
 	router.PUT("/tasks/:id", taskhandler.UpdateTask)
 	router.DELETE("/tasks/:id", taskhandler.DeleteTask)
-
-	url := ginSwagger.URL("swagger/doc.json")
-	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 
 	go rabbitmq.StartConsumer()
 
--- api/handler/handlers.go
+++ api/handler/handlers.go
@@ -19,19 +19,6 @@ func NewTaskHandler(db *mongodb.TaskMongoDb) *TaskHandler {
 	return &TaskHandler{db: db}
 }
 
-
-// @Router 			/tasks [post]
-// @Summary			CREATE TASKS
-// @Description 	This method creates tasks
-// @Security		BearerAuth
-// @Tags			TASK
-// @Accept 			json
-// @Produce			json
-// @Param 			body body models.Tasks true "Tasks"
-// @Success 201 {object} models.Tasks
-// @Failure 400 {object} gin.H{"error": "Invalid input"}
-// @Failure 500 {object} gin.H{"error": "Internal server error"}
-// @Router /tasks [post]
 func (t *TaskHandler) CreateTask(c *gin.Context) {
 	var task models.Tasks
 	if err := c.BindJSON(&task); err != nil {
@@ -51,15 +38,7 @@ func (t *TaskHandler) CreateTask(c *gin.Context) {
 
 	c.JSON(http.StatusCreated, task)
 }
-// GetTasks godoc
-// @Summary Get all tasks
-// @Description Get all tasks
-// @Tags tasks
-// @Accept  json
-// @Produce  json
-// @Success 200 {array} models.Tasks
-// @Failure 500 {object} gin.H{"error": "Internal server error"}
-// @Router /tasks [get]
+
 func (t *TaskHandler) GetTasks(c *gin.Context) {
 	tasks, err := t.db.StoreGetTasks()
 	if err != nil {
@@ -70,18 +49,6 @@ func (t *TaskHandler) GetTasks(c *gin.Context) {
 	c.JSON(http.StatusOK, tasks)
 }
 
-// UpdateTask godoc
-// @Summary Update a task
-// @Description Update a task by ID
-// @Tags tasks
-// @Accept  json
-// @Produce  json
-// @Param id path string true "Tasks ID"
-// @Param task body models.Tasks true "Tasks"
-// @Success 200 {object} models.Task
-// @Failure 400 {object} gin.H{"error": "Invalid input"}
-// @Failure 500 {object} gin.H{"error": "Internal server error"}
-// @Router /tasks/{id} [put]
 func (t *TaskHandler) UpdateTask(c *gin.Context) {
 	id := c.Param("id")
 	objectId, err := primitive.ObjectIDFromHex(id)
@@ -105,17 +72,6 @@ func (t *TaskHandler) UpdateTask(c *gin.Context) {
 	c.JSON(http.StatusOK, task)
 }
 
-// DeleteTask godoc
-// @Summary Delete a task
-// @Description Delete a task by ID
-// @Tags tasks
-// @Accept  json
-// @Produce  json
-// @Param id path string true "cdTask ID"
-// @Success 204
-// @Failure 400 {object} gin.H{"error": "Invalid task ID"}
-// @Failure 500 {object} gin.H{"error": "Internal server error"}
-// @Router /tasks/{id} [delete]
 func (t *TaskHandler) DeleteTask(c *gin.Context) {
 	id := c.Param("id")
 	objectId, err := primitive.ObjectIDFromHex(id)
